0172370703d062bb2cded93e65a1bea0d465b1b7
Math237 <richardgarcia28688@gmail.com>
2024-10-10 22:24:19 -0300

    ex1 10/10

---
+++ ex1.js
@@ -0,0 +1,10 @@
+/*
+Escreva um programa que solicite o valor do produto,
+ em seguida pergunte se deseja adicionar mais produtos, se for Sim ou S
+  ou 1 (escolha 1 deles), solicite novamente o produto.
+  Quando a resposta for não, apresente a soma dos valores do produto.
+  */
+ 
+  var produto = [];
+  var i = 0;
+var resposta = "SIM"
